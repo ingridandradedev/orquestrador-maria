6c98fa926c78a18be23d200e2607b3e38c8ff644
Ingrid Andrade <ingridandradeaguiar00@gmail.com>
2025-04-16 13:12:15 -0300

    database conection update

--- app/database.py
+++ app/database.py
@@ -6,9 +6,23 @@ from dotenv import load_dotenv
 # Carrega as variáveis do arquivo .env
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/postgres")
+# Parâmetros do pooler
+DB_HOST = "aws-0-sa-east-1.pooler.supabase.com"
+DB_PORT = 6543
+DB_NAME = "postgres"
+DB_USER = "postgres.sxzgjdqxmsdgfqjvzwak"
+DB_PASSWORD = ".vFVC29KtuY%YH2"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,  # Número máximo de conexões no pool
+    max_overflow=5,  # Número máximo de conexões extras
+    pool_timeout=30,  # Tempo limite para obter uma conexão
+    pool_pre_ping=True  # Verifica se a conexão está ativa antes de usá-la
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
